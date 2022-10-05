de690c9ba291a36cb493d6299c5cc549339499d2
Alexander Repeta <alexander.repeta@gmail.com>
2022-10-05 21:08:28 +0300

    add examples

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { RecipeList } from './RecipeList';
+import recipes from '../recipes.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <RecipeList recipes={recipes} />
+    </>
   );
 };
