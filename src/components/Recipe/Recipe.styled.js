9e44377ac2e4c7d004d48fb04b2bbd818f3c7f74
Alexander Repeta <alexander.repeta@gmail.com>
2022-10-07 21:16:39 +0300

    add examples

---
+++ src/components/Recipe/Recipe.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  padding: 8px;
+  border: 1px solid ${(p) => p.theme.colors.black};
+  border-radius: 4px;
+`;
+
+export const Title = styled.h2`
+  margin-bottom: 16px;
+`;
---
+++ src/components/RecipeList/RecipeList.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ListItem = styled.li`
+  :not(:last-child) {
+    margin-bottom: 8px;
+  }
+`;
