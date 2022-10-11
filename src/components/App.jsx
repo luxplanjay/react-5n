8d7ecde48df1feb9fd2ad335c1ae580308128a1a
Alexander Repeta <alexander.repeta@gmail.com>
2022-10-11 21:59:20 +0300

    prepare examples

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,8 @@
-import { RecipeList } from './RecipeList/RecipeList';
 import { PaymentWidget } from './PaymentWidget/PaymentWidget';
-import recipes from '../recipes.json';
 
 export const App = () => {
   return (
     <>
-      {/* <RecipeList recipes={recipes} /> */}
-      {/* <hr /> */}
       <PaymentWidget />
     </>
   );
--- src/components/RecipeList/RecipeList.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const List = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, 340px);
-  gap: 16px;
-`;
