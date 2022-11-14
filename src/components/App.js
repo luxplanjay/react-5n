65a644e6bf7907a20f5ae85851a5aff63f6281d6
Alexander Repeta <alexander.repeta@gmail.com>
2022-11-14 20:18:45 +0200

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,16 @@
-import { Dog } from './Dog';
+import { useEffect } from 'react';
+
+import { fetchSmething } from './api';
 import { GlobalStyle } from './GlobalStyle';
-import { BreedSelect } from './BreedSelect';
-import { DogSkeleton } from './DogSkeleton';
-import { ErrorMessage } from './ErrorMessage';
-import { Layout } from './Layout';
-import { fetchDogByBreed } from 'api';
-import { errorMessages } from 'constants';
-import { useState } from 'react';
 
 export const App = () => {
-  const [dog, setDog] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const selectBreed = async (breedId) => {
-    try {
-      setIsLoading(true);
-      const dog = await fetchDogByBreed(breedId);
-      setDog(dog);
-    } catch {
-      setError(errorMessages.fetchDog);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  useEffect(() => {
+    fetchSmething().then().catch();
+  }, []);
 
   return (
-    <Layout>
-      <BreedSelect onSelect={selectBreed} />
-      {error && <ErrorMessage>{error}</ErrorMessage>}
-      {isLoading && <DogSkeleton />}
-      {dog && !isLoading && <Dog dog={dog} />}
+    <>
       <GlobalStyle />
-    </Layout>
+    </>
   );
 };
