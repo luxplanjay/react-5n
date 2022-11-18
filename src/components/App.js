67469b79e65616c017913f200d04718c81cfd407
Alexander Repeta <alexander.repeta@gmail.com>
2022-11-18 19:29:52 +0200

    result

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,6 @@
-import { useEffect } from 'react';
-
-import { fetchSmething } from './api';
 import { GlobalStyle } from './GlobalStyle';
 
 export const App = () => {
-  useEffect(() => {
-    fetchSmething().then().catch();
-  }, []);
-
   return (
     <>
       <GlobalStyle />
